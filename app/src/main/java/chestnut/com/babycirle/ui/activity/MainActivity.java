bbde354d8cb08f6beb0860ca63edd22c594f2b51
宋正朋 <chestnutTang@163.com>
2018-01-09 16:18:57 +0800

    加载动画

--- app/src/main/java/chestnut/com/babycirle/ui/activity/MainActivity.java
+++ app/src/main/java/chestnut/com/babycirle/ui/activity/MainActivity.java
@@ -1,11 +1,20 @@
 package chestnut.com.babycirle.ui.activity;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.airbnb.lottie.Cancellable;
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieComposition;
 
 import butterknife.BindView;
 import chestnut.com.babycirle.R;
@@ -21,6 +30,8 @@ public class MainActivity extends BaseActivity {
     BottomNavigationView navigation;
     @BindView(R.id.container)
     ConstraintLayout container;
+    @BindView(R.id.animation_view)
+    LottieAnimationView animationView;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -48,6 +59,30 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        animationView.setImageAssetsFolder("peter/");
+        animationView.setAnimation("data.json");
+        animationView.loop(true);
+        // 任何符合颜色过滤界面的类
+        final PorterDuffColorFilter colorFilter = new PorterDuffColorFilter(Color.RED,
+                PorterDuff.Mode.LIGHTEN);
+//
+//// 在整个视图中添加一个颜色过滤器
+//        animationView.addColorFilter(colorFilter);
+        animationView.setColorFilter(colorFilter);
+        animationView.playAnimation();
+//        Cancellable compositionCancellable = LottieComposition.Factory.fromJson(getResources(),
+// jsonObject, (composition) -> {
+//            animationView.setComposition(composition);
+//            animationView.playAnimation();
+//        });
+        message.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animationView.cancelAnimation();
+                animationView.clearAnimation();
+//                animationView.setVisibility(View.GONE);
+            }
+        });
     }
 
     @Override
